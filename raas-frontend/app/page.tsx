5518be0e9ac02605df21c43e6d51be4679f4c221
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2026-03-12 00:03:35 -0400

    added prettier

--- raas-frontend/app/page.tsx
+++ raas-frontend/app/page.tsx
@@ -1,23 +1,21 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex justify-center items-center">
       <div className="hover-3d">
-  {/* content */}
-  <figure className="max-w-100 rounded-2xl">
-    <img src="/sk_icon.png" alt="3D card" />
-  </figure>
-  {/* 8 empty divs needed for the 3D effect */}
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-</div>
+        {/* content */}
+        <figure className="max-w-100 rounded-2xl">
+          <img src="/sk_icon.png" alt="3D card" />
+        </figure>
+        {/* 8 empty divs needed for the 3D effect */}
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
