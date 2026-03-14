f3aa6ca74f32c2a9462f9008bd8503f32be47acb
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2026-03-14 17:05:37 -0400

    moved db create request to get connection

--- api/core/db.py
+++ api/core/db.py
@@ -6,6 +6,7 @@ DB_PATH.parent.mkdir(parents=True, exist_ok=True)
 
 
 def get_conn() -> sqlite3.Connection:
+    DB_PATH.parent.mkdir(parents=True, exist_ok=True)
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
